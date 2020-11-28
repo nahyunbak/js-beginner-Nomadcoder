685726f6e9ead6caa90a121dac0de5d898025b34
nahyunbak <61341571+nahyunbak@users.noreply.github.com>
2020-11-28 18:12:30 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,6 +4,11 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+//1단계: todoList에서 할 일 넣기  
+//1) 요소 만들기: document.creatElement("h1")
+//2) 요소에 값 넣기:요소.value = "hey" ..placeholder면 또 다르겠지??
+//3) 요소 사이에 문자 넣기 : 요소.innerText = text;
+//4) 요소 밑에 아이 넣기: 요소.appendChild(span); 
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
@@ -15,6 +20,9 @@ function paintToDo(text){
     toDoList.appendChild(li);
 }
 
+//2단계: 
+//나머지 똑같음. 
+//todo값 입력했을 때 초기회됨
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
@@ -28,7 +36,7 @@ function loadToDos() {
 
 }
 function init() {
-  loadTodos();
+  loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
