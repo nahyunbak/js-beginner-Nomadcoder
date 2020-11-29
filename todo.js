f1c98bf9a9eaf2b1412e141ae14fb3e58f7c7b64
nahyunbak <61341571+nahyunbak@users.noreply.github.com>
2020-11-29 22:49:09 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -7,12 +7,36 @@ const TODOS_LS = 'toDos';
 const toDos = [];
 
 
+
+//-2단계: filter는, 마치 forEach에서 funtion을 실행하듯, 각각의 item들과 함께 실행될 것이다...
+//filter는 리턴값이 1인 것==true만 가지고 간다.
+
+function filterFn(toDo) {
+    return toDo.id === 1;
+}
+
+
+//-1단계: 화면에서 버튼과 li삭제하기  
+//event.target <<클릭된 버튼을 의미-타겟이다. 
+//정리하자면, console.dir(타겟 엘리먼트)로 부모를 찾고, 그 부모 '요소'를 삭제할 방법을 구글링.
+// 
+
+
+function deleteToDo(event){
+    const btn =event.target.parentNode;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(filterFn);
+    console.log("cleanToDos")
+}                          
+
 //0단계: 저장하기.
 //자바스크립트는 전부 문자열로 저장하려 한다. ...고로 바꿔야 할 필요가 있다.
 //JSON.stringfy는 오브젝트를 문자열로 바꿔서 저장해준다. 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
+
 
 //1단계: todoList에서 할 일 나오게 만들기  
 //1) 요소 만들기: document.creatElement("h1")
@@ -36,6 +60,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -66,6 +91,7 @@ function handleSubmit(event){
 //set과 get 아이템 메소드 인자로 쓰임. 
 //.forEach() 메소드는, for each괄호안에 있는 함수의 배열의 각 요소를 넣어 한 번씩 실행함. 
 //object.key 를 하면 밸류가 나온다. 
+//array도 function이 있고, string도 function이 있고, object도 function이 있따. 
 /*
 여기서, console.log(toDo.text)를 하면 text에 저장된 인풋값의 이름이 쫙 나온다. 
 function loadToDos() {
